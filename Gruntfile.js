9313b8d781b8b89706bb94e8bb12c93d323c96e6
Hack Reactor Students <accounts@hackreactor.com>
2016-11-30 18:55:56 -0800

    Added comments for testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,6 +82,7 @@ module.exports = function(grunt) {
   grunt.registerTask('upload', function(n) {
     if (grunt.option('prod')) {
       // add your production server task here
+      //extra line for testing
     } else {
       grunt.task.run([ 'server-dev' ]);
     }
